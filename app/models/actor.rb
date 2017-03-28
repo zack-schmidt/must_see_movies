8d1628ac5299a0738891a9208e83107b35d6db10
Raghu Betina <raghu@firstdraft.com>
2017-03-28 15:57:47 +0000

    Implement image uploads

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,6 @@
 class Actor < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   # Direct associations
 
   has_many   :castings,
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  mount_uploader :image, ImageUploader
+
   # Direct associations
 
   belongs_to :director
